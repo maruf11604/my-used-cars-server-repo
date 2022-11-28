3fdde9722beea0b367172d4e0540041c2fce9016
maruf <55664274+maruf11604@users.noreply.github.com>
2022-11-28 13:43:39 +0600

    seller api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 //middleware
 
@@ -23,10 +23,51 @@ async function run() {
       .db("usedProductsMarket")
       .collection("productOptions");
 
+    const bookingsCollection = client
+      .db("usedProductsMarket")
+      .collection("bookings");
+    const usersCollection = client.db("usedProductsMarket").collection("users");
+    const sellersCollection = client
+      .db("usedProductsMarket")
+      .collection("sellers");
+
     app.get("/productOptions", async (req, res) => {
       const query = {};
       const options = await productOptionCollection.find(query).toArray();
       res.send(options);
+    });
+
+    app.get("/productOptions/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const buying = await productOptionCollection.findOne(query);
+      res.send(buying);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.post("/sellers", async (req, res) => {
+      const user = req.body;
+      const result = await sellersCollection.insertOne(user);
+      res.send(result);
     });
   } finally {
   }
